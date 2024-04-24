6035f4de4ddfe9999357056fa08105c510194db8
Lindsay <136626682+Lindisinha@users.noreply.github.com>
2024-04-23 22:13:02 -0400

    Criando projeto
    Criei o projeto inicial de telas da atividade "projeto escola"

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace pds_projeto_escola
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
